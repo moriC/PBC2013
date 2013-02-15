11c1d14d2b779dbeed2e793265efa64cba4f4bd1
morishita <tomohiro.m0219@gmail.com>
2013-02-15 23:35:47 +0900

    set up Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PBC2013::Application.config.secret_token = '3fe86a3f662e1b295679675f0474d410c2c4fa276702c0ee54896eda54dd0277d691f7b9bea0427c7ccf22faa7111b47563843ddb39a90b65cd8f751664184df'
