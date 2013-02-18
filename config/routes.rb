ae2f52975c8f7cf231081bf6635ac3209f7903a6
morishita <tomohiro.m0219@gmail.com>
2013-02-18 11:13:21 +0900

    create logout button

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,6 @@ PBC2013::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  root :to => "home#index"
   resources :home
   resources :camp do
     collection do
