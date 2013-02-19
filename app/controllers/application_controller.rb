ef1ccacb4aec07bdb79cbb54faecf3acce1a90ab
morishita <tomohiro.m0219@gmail.com>
2013-02-19 13:24:55 +0900

    change devise redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   
   def after_sign_in_path_for(resource_or_scope)
-    "/"
+    root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    '/users/sign_in'
+    new_user_session_path
   end
 end
