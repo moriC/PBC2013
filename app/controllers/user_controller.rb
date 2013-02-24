443e6bb27563d4ae5b26841d1e31fd084420f9fc
morishita <tomohiro.m0219@gmail.com>
2013-02-24 13:14:43 +0900

    Output success message when the system success update user information

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class UserController < ApplicationController
   layout 'main'
 
@@ -11,7 +12,8 @@ class UserController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update_attributes(params[:user])
-  	  redirect_to @user
+      flash[:notice] = "ユーザ情報更新に成功しました。"
+  	  redirect_to action: :show
   	else
   	  render "/user/edit"
   	end
