6064106ecb4523aa0f3fc3dbb2e6295629d57fb2
morishita <tomohiro.m0219@gmail.com>
2013-03-01 23:57:22 +0900

    create give login test in cucumber

--- features/step_definitions/default_steps.rb
+++ features/step_definitions/default_steps.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 前提 /^参加者がログインしている$/ do
-
+  @user ||= FactoryGirl.create :user, number: "b210132" ,email: "b210132@cc.it-hiroshima.ac.jp", password: "hogehoge"
+  visit('/users/sign_in')
+  fill_in 'user[number]', with: @user.number
+  fill_in 'user[password]', with: "hogehoge"
+  click_button 'login'
 end
 
 もし /^"(.*?)"に訪問する$/ do |path|
