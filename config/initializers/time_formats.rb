4c47b8af05ce1f6cc3a447a9915ba4bd7380edd7
morishita <tomohiro.m0219@gmail.com>
2013-03-04 14:39:40 +0900

    change time formats

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+
+Time::DATE_FORMATS[:date_ja] = "%Y年%m月%d日"
+Time::DATE_FORMATS[:datetime_ja] = "%Y年%m月%d日 %H時%M分%S秒"
+Time::DATE_FORMATS[:time_ja] = "%H時%M分%S秒"
+Time::DATE_FORMATS[:simple] = "%Y/%m/%d %H:%M:%S"
+Time::DATE_FORMATS[:simple_date] = "%Y/%m/%d"
+Time::DATE_FORMATS[:simple_time] = "%H:%M"
