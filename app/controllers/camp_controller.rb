a4854475f3a2d9c7e19664a1704da2696e102051
morishita <tomohiro.m0219@gmail.com>
2013-04-01 23:46:08 +0900

    create registration camp function

--- app/controllers/camp_controller.rb
+++ app/controllers/camp_controller.rb
@@ -1,11 +1,24 @@
+#coding: utf-8
 class CampController < ApplicationController
   layout 'main'
   def about
   end
   
   def entry
+  	@camp = Camp.new
   end
 
-  def orientation
+  def create
+  	@camp = Camp.new(params[:camp])
+    @camp.user_id = current_user.id
+    if @camp.save
+      flash[:notice] = "予約手続き完了しました！"
+      redirect_to :controller => 'home'
+    else
+      format.html { render action: "new" }
+    end
+  end
+
+  def camp
   end
 end
